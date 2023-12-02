eeb96934d119e97b532b8fbb5c1ff6008d0790b7
inlake77 <148287936+inlake77@users.noreply.github.com>
2023-12-02 19:26:07 +0100

    Add files via upload

---
+++ Scripts/SoundtrackScripts/ChapterYellowSoundtrackLogic.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChapterYellowSoundtrackLogic : MonoBehaviour
+{
+    [SerializeField] Animator chapterYellowSoundtrackAnimation;
+    [SerializeField] GameObject chapterYellowSoundtrackOriginal;
+    private static bool chapterYellowSoundtrackIsPlaying = false;
+
+    private void Awake()
+    {
+        if (chapterYellowSoundtrackIsPlaying)
+            chapterYellowSoundtrackOriginal.SetActive(false);
+    }
+
+    private void Start()
+    {
+        if (!chapterYellowSoundtrackIsPlaying)
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            DontDestroyOnLoad(gameObject);
+            chapterYellowSoundtrackIsPlaying = true;
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Yellow To Red Transition")
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+            chapterYellowSoundtrackAnimation.SetBool("OffVolume", true);
+        }
+    }
+
+    private void DestroyChapterYellowSoundtrack()
+    {
+        chapterYellowSoundtrackIsPlaying = false;
+        Destroy(gameObject);
+    }
+}
